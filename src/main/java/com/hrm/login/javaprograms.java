18708d60779a1adf38150085f6f15079e93547f3
madhukiran4216 <madhukiran4216@gmail.com>
2020-03-12 23:49:10 +0530

    second commit

---
+++ src/main/java/com/hrm/login/javaprograms.java
@@ -0,0 +1,11 @@
+package com.hrm.login;
+
+public class javaprograms {
+	public static void factorial(int f) {
+		int fact = 0;
+		for (int i = 1; i < f; i++) {
+			fact = fact * i;
+		}
+		System.out.println(fact);
+}
+}
